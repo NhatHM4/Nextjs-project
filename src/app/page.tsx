788e3d7afa90d732af8132e5c73d2741db5fd9bb
NhatHM4 <haminhnhat711@gmail.com>
2024-12-30 21:20:34 +0700

    video 103

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,21 @@
 import MainSlider from "@/components/main/main.slider";
+import { sendRequest } from "@/utils/api";
 import { Container } from "@mui/material";
 
 export default async function HomePage() {
 
-  const res = await fetch('http://localhost:8000/api/v1/tracks/top', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      category: "CHILL",
-      limit: 2
-    }),
-  });
+  const res = await sendRequest<IBackendRes<ITrackTop[]>>(
+    {
+      url: 'http://localhost:8000/api/v1/tracks/top',
+      method: 'POST',
+      body: {
+        category: "CHILL",
+        limit: 2
+      }
+    }
+  );
 
-  console.log(" checkdata", await res.json());
+  // console.log(" checkdata", res?.data[1]?.title);
   return (
     <div>
       <Container>
