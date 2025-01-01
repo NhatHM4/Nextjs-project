946227730a3ff1590f6b02e4dc209fb033e6bb20
NhatHM4 <haminhnhat711@gmail.com>
2025-01-01 13:23:52 +0700

    video 113

--- src/components/track/wave.track.tsx
+++ src/components/track/wave.track.tsx
@@ -1,33 +1,9 @@
 
 'use client'
+import { useWavesurfer } from '@/utils/customHooks';
 import { useSearchParams } from 'next/navigation';
-import { useEffect, useMemo, useRef, useState } from 'react';
-import WaveSurfer from 'wavesurfer.js'
+import { useMemo, useRef } from 'react';
 
-
-// WaveSurfer hook
-const useWavesurfer = (containerRef: any, options: any) => {
-    const [wavesurfer, setWavesurfer] = useState<any>(null)
-
-    // Initialize wavesurfer when the container mounts
-    // or any of the props change
-    useEffect(() => {
-        if (!containerRef.current) return
-
-        const ws = WaveSurfer.create({
-            ...options,
-            container: containerRef.current,
-        })
-
-        setWavesurfer(ws)
-
-        return () => {
-            ws.destroy()
-        }
-    }, [options, containerRef])
-
-    return wavesurfer
-}
 
 const WaveTrack = () => {
     const searchParams = useSearchParams()
@@ -40,25 +16,8 @@ const WaveTrack = () => {
             url: `/api?audio=${fileName}`,
         }
     }, [fileName])
-    // const options = {
-    //     waveColor: 'rgb(200, 0, 200)',
-    //     progressColor: 'rgb(100, 0, 100)',
-    //     url: `/api?audio=${fileName}`,
-    // }
+
     const wavesurfer = useWavesurfer(containerRef, optionsMemo);
-
-
-
-    // useEffect(() => {
-
-    //     const wavesurfer = WaveSurfer.create({
-    //         container: ref.current as HTMLElement,
-    //         waveColor: 'rgb(146, 142, 146)',
-    //         progressColor: 'rgb(100, 0, 100)',
-    //         url: `/api?audio=${audio}`,
-    //     });
-    // }, []);
-
     return (
         <div ref={containerRef}>
             Wave Track
--- src/utils/customHooks.ts
+++ src/utils/customHooks.ts
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import WaveSurfer, { WaveSurferOptions } from "wavesurfer.js";
 
 const useHasMounted = () => {
     const [hasMounted, setHasMounted] = useState(false);
@@ -8,4 +9,30 @@ const useHasMounted = () => {
     return hasMounted;
 };
 
-export { useHasMounted };
+// WaveSurfer hook
+const useWavesurfer = (containerRef: React.RefObject<HTMLDivElement>
+    , options: Omit<WaveSurferOptions, 'container'>) => {
+    const [wavesurfer, setWavesurfer] = useState<any>(null)
+
+    // Initialize wavesurfer when the container mounts
+    // or any of the props change
+    useEffect(() => {
+        if (!containerRef.current) return
+
+        const ws = WaveSurfer.create({
+            ...options,
+            container: containerRef.current,
+        })
+
+        setWavesurfer(ws)
+
+        return () => {
+            ws.destroy()
+        }
+    }, [options, containerRef])
+
+    return wavesurfer
+}
+
+
+export { useHasMounted, useWavesurfer };
