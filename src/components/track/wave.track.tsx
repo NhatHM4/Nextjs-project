610627e560bfacfb16e908ded7d289baef084afe
NhatHM4 <haminhnhat711@gmail.com>
2025-01-01 14:28:36 +0700

    video 114

--- src/components/track/wave.track.tsx
+++ src/components/track/wave.track.tsx
@@ -2,13 +2,15 @@
 'use client'
 import { useWavesurfer } from '@/utils/customHooks';
 import { useSearchParams } from 'next/navigation';
-import { useMemo, useRef } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 
 
 const WaveTrack = () => {
+
     const searchParams = useSearchParams()
     const fileName = searchParams.get('audio')
     const containerRef = useRef<HTMLDivElement>(null);
+    const [isPlaying, setIsPlaying] = useState(false);
     const optionsMemo = useMemo(() => {
         return {
             waveColor: 'rgb(200, 0, 200)',
@@ -16,12 +18,35 @@ const WaveTrack = () => {
             url: `/api?audio=${fileName}`,
         }
     }, [fileName])
-
     const wavesurfer = useWavesurfer(containerRef, optionsMemo);
+
+    const onPlayPause = useCallback(() => {
+        wavesurfer && wavesurfer.playPause()
+        setIsPlaying(!isPlaying)
+    }, [wavesurfer])
+
+    useEffect(() => {
+        if (!wavesurfer) return
+        setIsPlaying(false)
+        const subscriptions = [
+            wavesurfer.on('play', () => setIsPlaying(true)),
+            wavesurfer.on('pause', () => setIsPlaying(false)),
+        ]
+        return () => {
+            subscriptions.forEach((unsub) => unsub())
+        }
+    }, [wavesurfer])
+
+
     return (
-        <div ref={containerRef}>
-            Wave Track
-        </div>
+        <>
+            <div ref={containerRef}>
+                Wave Track
+            </div>
+            <button onClick={onPlayPause} style={{ minWidth: '5em' }}>
+                {isPlaying ? 'Pause' : 'Play'}
+            </button>
+        </>
     );
 }
 
