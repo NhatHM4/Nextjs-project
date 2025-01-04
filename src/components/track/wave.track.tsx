7d3316bc7faa1d56d867e30b79be62e75330d716
NhatHM4 <haminhnhat711@gmail.com>
2025-01-04 16:59:46 +0700

    video 119

--- src/components/track/wave.track.tsx
+++ src/components/track/wave.track.tsx
@@ -14,30 +14,32 @@ const WaveTrack = () => {
     const containerRef = useRef<HTMLDivElement>(null);
     const timeEl = useRef<HTMLDivElement>(null);
     const durationEl = useRef<HTMLDivElement>(null);
+    const hoverEl = useRef<HTMLDivElement>(null);
     const [isPlaying, setIsPlaying] = useState(false);
 
-    // Create a canvas gradient
-    const canvas = document.createElement('canvas')
-    const ctx = canvas.getContext('2d')!
-    // Define the waveform gradient
-    const gradient = ctx.createLinearGradient(0, 0, 0, canvas.height * 1.35)
-    gradient.addColorStop(0, '#656666') // Top color
-    gradient.addColorStop((canvas.height * 0.7) / canvas.height, '#656666') // Top color
-    gradient.addColorStop((canvas.height * 0.7 + 1) / canvas.height, '#ffffff') // White line
-    gradient.addColorStop((canvas.height * 0.7 + 2) / canvas.height, '#ffffff') // White line
-    gradient.addColorStop((canvas.height * 0.7 + 3) / canvas.height, '#B1B1B1') // Bottom color
-    gradient.addColorStop(1, '#B1B1B1') // Bottom color
 
-    // Define the progress gradient
-    const progressGradient = ctx.createLinearGradient(0, 0, 0, canvas.height * 1.35)
-    progressGradient.addColorStop(0, '#EE772F') // Top color
-    progressGradient.addColorStop((canvas.height * 0.7) / canvas.height, '#EB4926') // Top color
-    progressGradient.addColorStop((canvas.height * 0.7 + 1) / canvas.height, '#ffffff') // White line
-    progressGradient.addColorStop((canvas.height * 0.7 + 2) / canvas.height, '#ffffff') // White line
-    progressGradient.addColorStop((canvas.height * 0.7 + 3) / canvas.height, '#F6B094') // Bottom color
-    progressGradient.addColorStop(1, '#F6B094') // Bottom color
 
     const optionsMemo: Omit<WaveSurferOptions, 'container'> = useMemo(() => {
+        // Create a canvas gradient
+        const canvas = document.createElement('canvas')
+        const ctx = canvas.getContext('2d')!
+        // Define the waveform gradient
+        const gradient = ctx.createLinearGradient(0, 0, 0, canvas.height * 1.35)
+        gradient.addColorStop(0, '#656666') // Top color
+        gradient.addColorStop((canvas.height * 0.7) / canvas.height, '#656666') // Top color
+        gradient.addColorStop((canvas.height * 0.7 + 1) / canvas.height, '#ffffff') // White line
+        gradient.addColorStop((canvas.height * 0.7 + 2) / canvas.height, '#ffffff') // White line
+        gradient.addColorStop((canvas.height * 0.7 + 3) / canvas.height, '#B1B1B1') // Bottom color
+        gradient.addColorStop(1, '#B1B1B1') // Bottom color
+
+        // Define the progress gradient
+        const progressGradient = ctx.createLinearGradient(0, 0, 0, canvas.height * 1.35)
+        progressGradient.addColorStop(0, '#EE772F') // Top color
+        progressGradient.addColorStop((canvas.height * 0.7) / canvas.height, '#EB4926') // Top color
+        progressGradient.addColorStop((canvas.height * 0.7 + 1) / canvas.height, '#ffffff') // White line
+        progressGradient.addColorStop((canvas.height * 0.7 + 2) / canvas.height, '#ffffff') // White line
+        progressGradient.addColorStop((canvas.height * 0.7 + 3) / canvas.height, '#F6B094') // Bottom color
+        progressGradient.addColorStop(1, '#F6B094') // Bottom color
         return {
             waveColor: gradient,
             progressColor: progressGradient,
@@ -73,6 +75,13 @@ const WaveTrack = () => {
                 }
             })
         ]
+        if (containerRef.current && hoverEl.current) {
+            containerRef.current.addEventListener('pointermove', (e) => {
+                if (hoverEl.current) {
+                    hoverEl.current.style.width = `${e.offsetX}px`;
+                }
+            })
+        }
         return () => {
             subscriptions.forEach((unsub) => unsub())
         }
@@ -93,6 +102,7 @@ const WaveTrack = () => {
                 Wave Track
                 <div ref={timeEl} className='time'>0:00</div>
                 <div ref={durationEl} className='duration'>0:00</div>
+                <div ref={hoverEl} className='hover'></div>
             </div>
             <button onClick={onPlayPause} style={{ minWidth: '5em' }}>
                 {isPlaying ? 'Pause' : 'Play'}
