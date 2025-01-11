af10908d5666836bc9dcb230c56118dec45cb95c
NhatHM4 <haminhnhat711@gmail.com>
2025-01-11 10:46:04 +0700

    video 134

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import { sendRequest } from "@/utils/api";
 import NextAuth, { AuthOptions } from "next-auth";
 import { JWT } from "next-auth/jwt";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 import GithubProvider from "next-auth/providers/github";
 
@@ -8,13 +9,42 @@ export const authOptions: AuthOptions = {
     secret: process.env.NO_SECRET,
     // Configure one or more authentication providers
     providers: [
+
+        CredentialsProvider({
+            // The name to display on the sign in form (e.g. "Sign in with...")
+            name: "website",
+            // `credentials` is used to generate a form on the sign in page.
+            // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+            // e.g. domain, username, password, 2FA token, etc.
+            // You can pass any HTML attribute to the <input> tag through the object.
+            credentials: {
+                username: { label: "Username", type: "text" },
+                password: { label: "Password", type: "password" }
+            },
+            async authorize(credentials, req) {
+                // Add logic here to look up the user from the credentials supplied
+                const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+
+                if (user) {
+                    // Any object returned will be saved in `user` property of the JWT
+                    return user
+                } else {
+                    // If you return null then an error will be displayed advising the user to check their details.
+                    return null
+
+                    // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+                }
+            }
+        }),
         GithubProvider({
             clientId: process.env.GITHUB_ID!,
             clientSecret: process.env.GITHUB_SECRET!,
-        }),
-        // ...add more providers here
+        })
     ],
     callbacks: {
+
+        // lấy token ở cookie mỗi khi có 1 request gửi lên next server rồi giả mã (decode), sau đó thì mình modify lại, thêm hay bớt data gì đó
+        // khi dùng useSession ở client thì nó sẽ chay tự động gửi token xuống để check
         async jwt({ token, user, account, profile, trigger }) {
             if (trigger === 'signIn' && account?.provider === 'github') {
                 const res = await sendRequest<IBackendRes<JWT>>({
@@ -26,10 +56,13 @@ export const authOptions: AuthOptions = {
                     token.access_token = res.data?.access_token;
                     token.refresh_token = res.data.refresh_token;
                     token.user = res.data.user;
+                    token.user.image = user?.image as string;
                 }
             }
             return token
         },
+
+        // sau khi modify cái token thì nạp ngược lại cho session
         session({ session, token, user }) {
             session.user = token.user
             session.access_token = token.access_token
--- src/app/types/next-auth.d.ts
+++ src/app/types/next-auth.d.ts
@@ -8,6 +8,7 @@ interface IUser {
     isVerify: boolean;
     type: string;
     role: string;
+    image?: string;
 }
 
 
