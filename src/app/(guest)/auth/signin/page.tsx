7a36a483a12adcb0b44c16112826589acce33f19
NhatHM4 <haminhnhat711@gmail.com>
2025-02-11 21:49:17 +0700

    practice done

---
+++ src/app/(guest)/auth/signin/page.tsx
@@ -0,0 +1,16 @@
+import AuthSignIn from "@/components/auth/auth.signin"
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+
+const SignInPage = async () => {
+    const session = await getServerSession(authOptions);
+    if (session) {
+        redirect('/')
+    }
+    return (
+        <AuthSignIn />
+    )
+}
+
+export default SignInPage;
