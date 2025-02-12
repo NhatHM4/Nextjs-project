54611fb339bb4bc8d3e1216e98b7a6c31c01a771
NhatHM4 <haminhnhat711@gmail.com>
2025-02-12 21:21:28 +0700

    video 147

---
+++ src/app/(user)/track/upload/page.tsx
@@ -0,0 +1,13 @@
+import UploadTab from "@/components/track/upload.tab";
+import { Container } from "@mui/material";
+
+const UploadPage = () => {
+
+    return (
+        <Container>
+            <UploadTab />
+        </Container>
+    );
+}
+
+export default UploadPage;
